791a7ef8373c77372b054b42cdef470dd9edd63b
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-09-26 14:11:06 -0500

    Updating CSS

--- views/index.php
+++ views/index.php
@@ -1,5 +1,4 @@
 <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl.$asset; ?>/css/card.css" />
-
 <div id="<?php echo $model->scriptName; ?>" data-attr-id="<?php echo $model->id; ?>">
 	<div class="card-header">
 		<div class="title pull-left">Twitter Stats</div>
