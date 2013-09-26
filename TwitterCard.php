7fe722c79f0a163dc200814af566209ba24b9117
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-09-26 09:11:26 -0500

    Retrieving and caching information from twitter. Still need to work on display

--- TwitterCard.php
+++ TwitterCard.php
@@ -16,10 +16,47 @@ class TwitterCard extends CiiCard
 		);
 	}
 
-	public functon attributeLabels()
+	public function attributeLabels()
 	{
 		return array(
 			'username' => 'Twitter Username'
 		);
 	}
+
+	/**
+     * getTweets callback method
+     * @param  $_POST  $postData Data supplied over post
+     */
+    public function getTwitterDetails()
+    {
+     	header("Content-Type: application/json");
+
+        Yii::import('ext.twitteroauth.*');
+
+        try {
+            $connection = new TwitterOAuth(
+            	Cii::getConfig('ha_twitter_key', NULL, NULL),
+				Cii::getConfig('ha_twitter_secret', NULL, NULL),
+				Cii::getConfig('ha_twitter_accessToken', NULL, NULL),
+				Cii::getConfig('ha_twitter_accessTokenSecret', NULL, NULL)
+		);
+
+		$user = Yii::app()->cache->get($this->scriptName . $this->id . $this->username . '_user_info');
+
+		if ($user == false)
+		{
+			$user = $connection->get("https://api.twitter.com/1.1/users/show.json?screen_name={$this->username}");
+		
+			// Cache the result for 15 minutes
+			if (!isset($tweets->errors))
+				Yii::app()->cache->set($this->scriptName . $this->id . $this->username . '_user_info', $user, 900);
+		}
+
+		echo CJSON::encode($user);
+
+		} catch (Exception $e) {
+			echo CJSON::encode(array('errors' => array(array('message' => $e->getMessage()))));
+		}
+	}
+
 }
--- assets/js/card.js
+++ assets/js/card.js
@@ -1,3 +1,25 @@
 var n0PT08x5ZkZ7Eb9 = {
 
+	id : null,
+
+	uuid : null,
+
+	/**
+         * Load function is the bootstrap method all cards inherit.
+         * 
+         * @param  string id  The unique ID of the card we want this card to manipulate
+         */
+        load : function(id) {
+                // Set some variables
+                this.id = id;
+                this.uuid = "n0PT08x5ZkZ7Eb9-" + this.id;
+
+		this.getUserInfo();
+        },
+
+	getUserInfo : function() {
+		$.get(CiiDashboard.endPoint + "/card/callmethod/id/" + this.id + "/method/getTwitterDetails", function(data) {
+			console.log(data);
+		});
+	}
 };
