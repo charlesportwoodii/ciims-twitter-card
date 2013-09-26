4d3e85ae3fab0b07bda3dc5bae59ee9ec1c89210
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-09-26 12:23:09 -0500

    Showing some useful twitter information now.

--- assets/js/card.js
+++ assets/js/card.js
@@ -4,6 +4,8 @@ var n0PT08x5ZkZ7Eb9 = {
 
 	uuid : null,
 
+	name : null,
+
 	/**
          * Load function is the bootstrap method all cards inherit.
          * 
@@ -13,13 +15,22 @@ var n0PT08x5ZkZ7Eb9 = {
                 // Set some variables
                 this.id = id;
                 this.uuid = name + "-" + this.id;
+		this.name = name;
 
-		this.getUserInfo();
+		var self = this;
+		this.getUserInfo(self);
         },
 
-	getUserInfo : function() {
+	getUserInfo : function(self) {
 		$.get(CiiDashboard.endPoint + "/card/callmethod/id/" + this.id + "/method/getTwitterDetails", function(data) {
 			console.log(data);
+			$("#" + self.name).find(".user-info").css("background", "url(" + data.profile_banner_url + "/web)");
+			$("#" + self.name).find(".user-image").attr("src", data.profile_image_url_https);
+			$("#" + self.name).find("span.name").text(data.name);
+			$("#" + self.name).find("span.description").text(data.description);
+			$("#" + self.name).find("#followers").text(data.followers_count);
+			$("#" + self.name).find("#tweets").text(data.statuses_count);
+			$("#" + self.name).find("#following").text(data.friends_count);
 		});
 	}
 };
--- views/index.php
+++ views/index.php
@@ -2,9 +2,29 @@
 
 <div id="<?php echo $model->scriptName; ?>" data-attr-id="<?php echo $model->id; ?>">
 	<div class="card-header">
+		<div class="title pull-left">Twitter Stats</div>
+		<div class="title pull-right" id="user">@<?php echo Cii::get($model->username, null); ?></div>
 		<div class="clearfix"></div>
 	</div>
-
 	<div class="card-body">
+		<div class="user-info">
+			<img class="user-image rounded-img" src="data:null" />
+			<span class="name"></span>
+			<span class="description"></span>
+		</div>
+		<div class="details pull-right">
+			<div class="section">
+				<span class="count" id="followers"></span>
+				<span>Followers</span>
+			</div>
+			<div class="section">
+				<span class="count" id="tweets"></span>
+				<span>Tweets</span>
+			</div>
+			<div class="section">
+				<span class="count" id="following"></span>
+				<span>Following</span>
+			</div>
+		</div>
 	</div>
 </div>
